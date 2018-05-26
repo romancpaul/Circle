0ccba1fbb2099460a92dff29b5c3f571e8f607a3
Paul <romancpaul@gmail.com>
2018-05-26 09:02:27 +0300

    Circle

---
+++ src/com/HomeWork1/Main.java
@@ -0,0 +1,14 @@
+package com.HomeWork1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int radius = 3;
+	    double area = 3.14 * (radius * radius);
+        System.out.println("The area of circle is: " + area);
+
+        double circumference = 3.14 * 2 * radius;
+
+        System.out.println("The circumference of the circle is: " + circumference);
+    }
+}
